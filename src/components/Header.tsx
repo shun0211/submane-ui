3912cbca46bb6b8a963e58a6676af2e4507762d2
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-05-29 07:37:13 +0900

    ⚡️ ログアウトの機能追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,37 @@
 import { Menu, Avatar, UnstyledButton } from "@mantine/core";
+import axios from "axios";
+import { useRouter } from "next/router";
 import React, { forwardRef, useRef } from "react";
-
-// eslint-disable-next-line react/display-name
-const UserButton = forwardRef((props, ref) => (
-  <UnstyledButton
-    ref={ref}
-    sx={(theme) => ({
-      display: "block",
-      width: "100%",
-      padding: theme.spacing.sm,
-    })}
-    {...props}
-  >
-    <Avatar src="/icon_dammy.png" alt="avatar" />
-  </UnstyledButton>
-));
 
 const Header = () => {
   const ref = useRef();
+  const router = useRouter();
+  // eslint-disable-next-line react/display-name
+  const UserButton = forwardRef((props, ref) => (
+    <UnstyledButton
+      ref={ref}
+      sx={(theme) => ({
+        display: "block",
+        width: "100%",
+        padding: theme.spacing.sm,
+      })}
+      {...props}
+    >
+      <Avatar src="/icon_dammy.png" alt="avatar" />
+    </UnstyledButton>
+  ));
+
+  const handleLogout = async () => {
+    const res = await axios
+      .delete("http://localhost:1323/logout")
+      .catch((error) => console.log(error));
+    router.push("/");
+  };
 
   return (
     <div className="flex justify-end">
       <Menu control={<UserButton ref={ref} />}>
-        {<Menu.Item>ログアウト</Menu.Item>}
+        {<Menu.Item onClick={handleLogout}>ログアウト</Menu.Item>}
       </Menu>
     </div>
   );
