e0fab5c7e0af0cac60e7b2846acd11adb361a145
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-05-30 20:35:27 +0900

    ✨ サブスクリプション追加モーダル作成

--- src/components/Button/Add.tsx
+++ src/components/Button/Add.tsx
@@ -1,20 +1,68 @@
 import React, { useState } from "react";
-import { Button, Group, Modal } from "@mantine/core";
+import {
+  Box,
+  Button,
+  Group,
+  Modal,
+  NumberInput,
+  TextInput,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { TimeInput } from "@mantine/dates";
 
 const Add = () => {
   const [opened, setOpened] = useState(false);
+
+  const form = useForm({
+    initialValues: {
+      name: "",
+      contractAt: "",
+      price: 0,
+    },
+  });
 
   return (
     <div>
       <Modal
         opened={opened}
         onClose={() => setOpened(false)}
-        title="Introduce yourself!"
-        size="lg">
+        title="サブスクリプションを登録する"
+        size="lg"
+        classNames={{header: "justify-center relative", title: 'text-xl', close: 'absolute right-3',}}
+      >
+        <Box sx={{ maxWidth: 400 }} mx="auto">
+          <form onSubmit={form.onSubmit((values) => console.log(values))}>
+            <TextInput
+              required
+              data-autofocus
+              label="サブスクリプション名"
+              placeholder="Amazon Prime"
+              {...form.getInputProps("name")}
+            />
+            <NumberInput
+              mt="sm"
+              required
+              label="月額料金"
+              placeholder="1000"
+              {...form.getInputProps("price")}
+            />
+            <TimeInput label="契約日" />
+            <Group position="right" mt="md">
+              <Button type="submit">登録</Button>
+            </Group>
+          </form>
+        </Box>
       </Modal>
 
       <Group position="center">
-        <Button onClick={() => setOpened(true)} color="yellow" radius="xl" size="xl">Open Modal</Button>
+        <Button
+          onClick={() => setOpened(true)}
+          color="yellow"
+          radius="xl"
+          size="xl"
+        >
+          Add
+        </Button>
       </Group>
     </div>
   );
